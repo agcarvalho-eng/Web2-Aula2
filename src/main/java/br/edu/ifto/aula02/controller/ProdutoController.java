ab855511f0c1946f7da9e2d5b7027f1d64299b90
agcarvalho.eng <agcarvalho.eng@gmail.com>
2024-02-14 17:27:18 -0300

    Versão final - 14-02-2024 - 17:27hs.

--- src/main/java/br/edu/ifto/aula02/controller/ProdutoController.java
+++ src/main/java/br/edu/ifto/aula02/controller/ProdutoController.java
@@ -1,9 +1,12 @@
 package br.edu.ifto.aula02.controller;
 
 import br.edu.ifto.aula02.model.dao.ProdutoDAO;
+import br.edu.ifto.aula02.model.entity.Produto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,10 +20,50 @@ public class ProdutoController {
         dao = new ProdutoDAO();
     }
 
+    /**
+     * @param produto necessário devido utilizar no form.html o th:object que faz referência ao objeto esperado no controller.
+     * @return
+     */
+    @GetMapping("/form")
+    public String form(Produto produto){
+        return "/produto/form";
+    }
+
     @GetMapping("/list")
     public ModelAndView listar(ModelMap model) {
         model.addAttribute("produtos", dao.buscarProdutos());
-        return new ModelAndView("/produto/list"); //aponta o caminho da view no projeto em /templates/produto
+        return new ModelAndView("/produto/list"); //Aponta o caminho da view no projeto em /templates/produto.
+    }
+    @PostMapping("/save")
+    public ModelAndView save(Produto produto) {
+        dao.save(produto);
+        return new ModelAndView("redirect:/produto/list");
     }
 
+    /**
+     * @param id
+     * @return
+     * @PathVariable é utilizado quando o valor da variável é passada diretamente na URL.
+     */
+    @GetMapping("/remove/{id}")
+    public ModelAndView remove(@PathVariable("id") int id){
+        dao.remove(id);
+        return new ModelAndView("redirect:/produto/list"); //Aponta o caminho da view no projeto em /templates/produto.
+    }
+    /**
+     * @param id
+     * @return
+     * @PathVariable é utilizado quando o valor da variável é passada diretamente na URL
+     */
+    @GetMapping("/edit/{id}")
+    public ModelAndView edit(@PathVariable("id") int id, ModelMap model) {
+        model.addAttribute("produto", dao.buscarProduto(id));
+        return new ModelAndView("/produto/form", model);
+    }
+
+    @PostMapping("/update")
+    public ModelAndView update(Produto produto) {
+        dao.update(produto);
+        return new ModelAndView("redirect:/produto/list"); //Aponta o caminho da view no projeto em /templates/produto.
+    }
 }
--- src/main/java/br/edu/ifto/aula02/model/dao/ProdutoDAO.java
+++ src/main/java/br/edu/ifto/aula02/model/dao/ProdutoDAO.java
@@ -14,22 +14,22 @@ import java.util.logging.Logger;
 
 public class ProdutoDAO {
 
-    //criar um objeto Connection para receber a conexão
+    // Criando um objeto Connection para receber a conexão.
     Connection con;
 
     public ProdutoDAO(){
         con = MinhaConexao.conexao();
     }
 
-    // Buscar produto pelo id.
+    // Buscando produto pelo id.
     public Produto buscarProduto(int id) {
         try {
-            //comando sql
+            // Comando sql para buscar o produto com o id informado.
             String sql = "select * from produto where id = ?";
             PreparedStatement ps = con.prepareStatement(sql);
-            //referênciar o parâmetro do método para a ?
+            // Referenciando o parâmetro do método para a "?".
             ps.setInt(1, id);
-            //ResultSet, representa o resultado do comando SQL
+            // ResultSet, representa o resultado do comando SQL
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 Produto p = new Produto();
@@ -44,31 +44,85 @@ public class ProdutoDAO {
         return null;
     }
 
-    // Buscar todos os produtos cadastrados.
+    // Buscar todos os produtos cadastrados na tabela Produto.
     public List<Produto> buscarProdutos() {
         try {
-            //comando sql
+            // Comando sql para buscar todos os produtos da tabela.
             String sql = "select * from produto";
             PreparedStatement ps = con.prepareStatement(sql);
-            //ResultSet, representa o resultado do comando SQL.
+            // ResultSet, representa o resultado do comando SQL.
             ResultSet rs = ps.executeQuery();
-            //cria uma lista de pessoas para retornar
+            // Criando uma lista de produtos para retornar.
             List<Produto> produtos = new ArrayList();
-            //laço para buscar todas os produtos do banco.
+            // Laço para buscar todas os produtos do banco.
             while (rs.next()) {
                 Produto p = new Produto();
                 p.setId(rs.getInt("id"));
                 p.setDescricao(rs.getString("descricao"));
                 p.setValor(rs.getDouble("valor"));
-                //adicionar produto "p" na lista de produtos.
+                // Sdicionando produto "p" na lista de produtos.
                 produtos.add(p);
             }
-            //retorna a lista de produtos.
+            // Retornando a lista de produtos.
             return produtos;
         } catch (SQLException ex) {
             Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
+    }
+
+    // Remover um produto da tabela Produto pelo id.
+    public boolean remove(int id) {
+        try {
+            // Comando sql para deletar um produto pelo id.
+            String sql = "delete from produto where id = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            // Referenciando o parâmetro do método para a "?".
+            ps.setInt(1, id);
+            if(ps.executeUpdate() == 1)
+                return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    // Salvar um produto na tabela Produto.
+    public boolean save(Produto produto) {
+        try {
+            // Comando sql para salvar um produto.
+            String sql = "insert into produto (descricao, valor) values (?, ?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+            // Referenciando os parâmetros do método para "?".
+            ps.setString(1, produto.getDescricao());
+            ps.setDouble(2, produto.getValor());
+
+            if(ps.executeUpdate() == 1)
+                return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    // Update um produto na tabela Produto.
+    public boolean update(Produto produto) {
+        try {
+            // Comando sql para atualizar um produto.
+            String sql = "update produto set descricao=? and valor=? where id=?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            // Referenciando os parâmetros do método para as "?".
+            ps.setString(1, produto.getDescricao());
+            ps.setDouble(2, produto.getValor());
+            ps.setInt(3, produto.getId());
+
+            if(ps.executeUpdate() == 1)
+                return true;
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 
 }
